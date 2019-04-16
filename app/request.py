6a2d8ec12264c1fbf9a63796c8f2e8db7df00ebb
john victor <johnvictor0002@gmail.com>
2019-04-16 10:44:40 +0300

    added the process results function that takes the news results and changes them to a list

--- app/request.py
+++ app/request.py
@@ -36,3 +36,24 @@ def get_news(country,category):
 
     return news_result
 
+def process_results(news_list):
+    '''
+    function that takes in the movie results and transform them to a list
+    '''
+    news_result =[]
+ 
+    for news_item in news_list:
+        id = news_item.get('id')
+        name = news_item.get('name')
+        print(id)
+        title = news_item.get('title')
+        description = news_item.get('description')
+        url = news_item.get('url')
+        category = news_item.get('category')
+        country = news_item.get('country')
+
+        if url:
+            news_object = Source(id,name,title,description,url,category,country)
+            news_result.append(news_object)
+    return news_result
+
