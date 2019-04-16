98fb2782d0dca35bbf51cd25d007426b7c4b1ec7
john victor <johnvictor0002@gmail.com>
2019-04-16 10:40:17 +0300

    added the class source that defines the sources of the news

--- app/models.py
+++ app/models.py
@@ -14,3 +14,15 @@ class Articles:
         self.publishedAt =publishedAt
 
 
+class Source:
+    '''
+    define source
+            '''
+    def __init__(self, id, name, description, url, category, country,title):
+        self.id = id
+        self.name = name
+        self.description = description  
+        self.url = url
+        self.category = category
+        self.title =title
+        self.country= country
