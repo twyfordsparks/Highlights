c54d7594d2276832847251f0b84830e98ae5f82c
john victor <johnvictor0002@gmail.com>
2019-04-16 10:39:21 +0300

    added the class article that defines new objects

---
+++ app/models.py
@@ -0,0 +1,16 @@
+class Articles:
+    '''
+    define news objects
+    '''
+
+    def __init__(self,d,name,author,title,description,url,urlToImage,publishedAt):
+        self.id = id
+        self.name =name
+        self.author =author
+        self.title =title
+        self.description =description
+        self.url =url
+        self.urlToImage =urlToImage
+        self.publishedAt =publishedAt
+
+
